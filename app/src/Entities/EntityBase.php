f8f3831d0b1cf0dcb900b98f725af090d3b8bed6
boubleg <ruslan.khaibullin@gmail.com>
2016-02-08 01:20:56 +0100

    Missing phpDocs and small fixes

--- app/src/Entities/EntityBase.php
+++ app/src/Entities/EntityBase.php
@@ -2,6 +2,10 @@
 
 namespace KhaibullinTest\Entities;
 
+/**
+ * Class EntityBase
+ * @package KhaibullinTest\Entities
+ */
 abstract class EntityBase
 {
     public function __construct()
--- app/src/db/DBManagerInterface.php
+++ app/src/db/DBManagerInterface.php
@@ -2,6 +2,10 @@
 
 namespace KhaibullinTest\db;
 
+/**
+ * Interface DBManagerInterface
+ * @package KhaibullinTest\db
+ */
 interface DBManagerInterface
 {
     public static function query(string $sql);
